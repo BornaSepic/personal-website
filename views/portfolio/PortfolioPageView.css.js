936d5b62a497817087c73ceeb80519bea5cac8df
Borna Sepic <bornasepic98@gmail.com>
2020-01-21 17:37:17 +0100

    :page_facing_up: Added content for the portfolio page

--- views/portfolio/PortfolioPageView.css.js
+++ views/portfolio/PortfolioPageView.css.js
@@ -3,4 +3,24 @@ import styled from "styled-components";
 export const PortfolioPageContainer = styled('div')`
     height: 100%;
     width: 100%;
+    
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    
+    @media only screen and (max-width: 1120px) {
+        flex-flow: column;
+        justify-content: space-around;
+        padding: 0 0 0 2vw;
+    }  
+`;
+
+export const PortfolioInfo = styled('div')`
+    max-width: 500px;
+`;
+
+export const LogosWrapper = styled('div')`
+    svg {
+        max-width: 100%;
+    }
 `;
--- views/portfolio/PortfolioPageView.jsx
+++ views/portfolio/PortfolioPageView.jsx
@@ -1,9 +1,29 @@
-import IntroText from "../../components/intro-text/IntroText";
 import * as Styled from "./PortfolioPageView.css";
+import Portfolio from "./components/Portfolio";
 
 const PortfolioPageView = (props) => (
     <Styled.PortfolioPageContainer>
-        <IntroText/>
+        <Styled.PortfolioInfo>
+            <h1>Portfolio</h1>
+            <p>
+                Over the years, I’ve had the pleasure to work with dozens of really great people and companies on some
+                really amazing projects.
+            </p>
+            <br/>
+            <p>
+                Here are a few clients who I lay claim to working with. Because of the type of
+                clients I primarily work with, I’m not able to display some of my best work on this site.
+            </p>
+            <br/>
+            <p>If you would like
+                to see some examples or perhaps something a little more specific, please get in touch and I’d be happy
+                to
+                send along a few examples!
+            </p>
+        </Styled.PortfolioInfo>
+        <Styled.LogosWrapper>
+            <Portfolio />
+        </Styled.LogosWrapper>
     </Styled.PortfolioPageContainer>
 );
 
